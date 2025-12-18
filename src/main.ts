c3dee893a7df1f7b3756ccfb4c05c2f98fe6ffbf
Sparkah <timopro16@gmail.com>
2025-12-18 10:57:45 +0000

    upd infra to cloudflare

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
+//https://f2b8f313.basefrontendbuild.pages.dev/
+/*async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
-/*
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { join } from 'path';
-import { NestExpressApplication } from '@nestjs/platform-express';
+bootstrap();*/
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  app.useStaticAssets(join(__dirname, '..', 'public'));
+  const app = await NestFactory.create(AppModule);
+  
+  app.enableCors({
+    origin: [
+      'https://f2b8f313.basefrontendbuild.pages.dev/',
+      'http://localhost:7456',
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
   await app.listen(process.env.PORT || 3000);
 }
-bootstrap();*/
+bootstrap();
